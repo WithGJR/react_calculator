d5421956ae38e7dc13954f5698383262a606900c
WithGJR <kevin3372000@gmail.com>
2018-06-15 11:54:19 +0800

    finish basic functionalities

--- App.js
+++ App.js
@@ -10,40 +10,94 @@ import {
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      result: 0,
+    this.state = {
+      showResult: false, 
       operand: '',
       operator: '',
-      operandStack: []
+      temp: []
     };
     this.inputOperand = this.inputOperand.bind(this);
     this.changeOperator = this.changeOperator.bind(this);
+    this.inputDot = this.inputDot.bind(this);
   }
 
   inputOperand(n) {
     if (this.state.operand === '0' && n === '0') {
       return;
     }
-    if (this.state.operand === '0' && n !== '0') {
-      this.setState({ operand: n });
+    if (this.state.showResult) {
+      this.setState({ temp: [n], showResult: false, operand: n });
       return;
     }
-    this.setState({ operand: this.state.operand+n });
+
+    var newOperand = (this.state.operand === '0' && n !== '0') ? n : this.state.operand+n;
+    var newTemp = this.state.temp.slice();
+    if (newTemp.length > 0) {
+      var lastElement = newTemp[newTemp.length - 1];
+      if (!isNaN(lastElement)) {
+        newTemp[newTemp.length - 1] = newOperand;
+      } else if (lastElement === '.') {
+        newOperand = n;
+        newTemp.push(n);
+      } else {
+        newTemp.push(newOperand);
+      }
+    } else {
+      newTemp.push(newOperand);
+    }
+    
+    this.setState({ operand: newOperand, temp: newTemp });
   }
 
   changeOperator(operator) {
-    if (this.operand !== '') {
-      this.setState({ operandStack: this.state.operandStack.concat(operand) });
+    if (this.state.showResult) {
+      this.setState({ temp: [operator], showResult: false, operand: newOperand });
     }
 
-    this.setState({ operator });
+    var newTemp = this.state.temp.slice();
+    if (newTemp.length > 0) {
+      var lastElement = newTemp[newTemp.length - 1];
+      if (isNaN(lastElement)) {
+        newTemp[newTemp.length - 1] = operator;
+      } else {
+        newTemp.push(operator);
+      }
+    }
+
+    if (this.state.operand !== '') {
+      this.setState({ operand: '', operator, temp: newTemp });
+    } else {
+      this.setState({ operator, temp: newTemp });
+    }
+  }
+
+  inputDot() {
+    if (this.state.showResult) {
+      this.setState({ temp: [], showResult: false, operand: '', operator: '' });
+      return;
+    }
+
+    var newTemp = this.state.temp.slice();
+    if (newTemp.length > 0) {
+      var lastElement = newTemp[newTemp.length - 1];
+      if (lastElement === '.') {
+        return;
+      }
+      newTemp.push('.');
+    }
+    this.setState({ temp: newTemp });
   }
 
   render() {
+    var result = this.state.temp.join('');
+    if (this.state.showResult) {
+      result += '=' + eval(result);
+    }
+
     return (
       <View style={styles.container}>
         <View style={styles.numberTextSection}>
-          <Text style={styles.numberText}>{this.state.operand}</Text>
+          <Text style={styles.numberText}>{result}</Text>
         </View>
 
         <View style={styles.buttonsContainer}>
@@ -72,7 +126,7 @@ export default class App extends Component {
             <TouchableOpacity style={styles.numberButton} onPress={() => { this.inputOperand('6'); }}>
               <Text stlye={styles.numberButtonText}>6</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.operationButton} onPress={() => { this.changeOperator('x'); }}>
+            <TouchableOpacity style={styles.operationButton} onPress={() => { this.changeOperator('*'); }}>
               <Text stlye={styles.operationButtonText}>x</Text>
             </TouchableOpacity>
           </View>
@@ -93,11 +147,14 @@ export default class App extends Component {
           </View>
 
           <View style={styles.buttonRowContainer}>
-              <TouchableOpacity style={styles.halfButton} onPress={() => { this.inputOperand('0'); }}>
+              <TouchableOpacity style={styles.numberButton} onPress={() => { this.inputOperand('0'); }}>
                 <Text stlye={styles.numberButtonText}>0</Text>
               </TouchableOpacity>
-              <TouchableOpacity style={styles.quarterButton} onPress={() => {}}>
+              <TouchableOpacity style={styles.numberButton} onPress={() => { this.inputDot(); }}>
                 <Text stlye={styles.numberButtonText}>.</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.operationButton} onPress={() => { this.setState({ showResult: true }); }}>
+                <Text stlye={styles.operationButtonText}>=</Text>
               </TouchableOpacity>
               <TouchableOpacity style={styles.operationButton} onPress={() => { this.changeOperator('+'); }}>
                 <Text stlye={styles.operationButtonText}>+</Text>
